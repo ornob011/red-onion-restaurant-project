5d175a0c06f4098707a66f3a80f50a2de8e5f361
Ornob Rahman <54552597+ornob011@users.noreply.github.com>
2020-03-24 23:32:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './components/Page/HomePage';
+import SignUp from '../src/components/auth/SignUp/SignUp'
+import Login from '../src/components/auth/Login/Login'
+import Cart from './components/Cart/Cart';
+import NotFoundPage from './components/Page/NotFoundPage';
+import FoodDetails from './components/Food/FoodDetails';
+import { UserProvider } from './components/auth/useAuth';
+import UserProfile from './components/Page/UserProfile';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Checkout from './components/Cart/Checkout';
+import Foods from './components/Page/Foods';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <UserProvider>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+                <PrivateRoute path="/cart">
+                    <Cart />
+                </PrivateRoute>
+                <PrivateRoute path="/checkout">
+                    <Checkout />
+                </PrivateRoute>
+                <Route exact path="/foods/" component={Foods} />
+                <Route path="/foods/:id" component={FoodDetails} />
+                <PrivateRoute path="/user/profile">
+                    <UserProfile />
+                </PrivateRoute>
+                <Route path="*" component={NotFoundPage} />
+            </Switch>
+        </UserProvider>
+    );
 }
 
 export default App;
---
+++ src/components/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAESwhoYHAJOno6c9GeHwXl0SQ8S2hqw2w",
+    authDomain: "red-onion-restaurant-project.firebaseapp.com",
+    databaseURL: "https://red-onion-restaurant-project.firebaseio.com",
+    projectId: "red-onion-restaurant-project",
+    storageBucket: "red-onion-restaurant-project.appspot.com",
+    messagingSenderId: "355668255685",
+    appId: "1:355668255685:web:c10354fe5923c2d4fae1ac"
+};
+
+export default firebaseConfig;
